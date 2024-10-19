39a572c1d45ac8f72b5179424b08dc7cf139df80
MidnightWolf420 <92683505+MidnightWolf420@users.noreply.github.com>
2024-10-19 18:54:36 -0400

    Rename

--- chrome/background.js
+++ chrome/background.js
@@ -1,12 +1,22 @@
 const targetURL = "https://bsky.app/";
 const twitterRegex = /https?:\/\/(x|twitter)\.com/;
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && twitterRegex.test(tab.url)) {
-    const parsedUrl = new URL(tab.url);
-    const params = new URLSearchParams(parsedUrl.search);
-    if (!(params.get('no-redirect') || params.get('no-redirect') === 'true')) {
-      chrome.tabs.update(tabId, { url: targetURL });
+chrome.webRequest.onBeforeRequest.addListener(
+  async (details) => {
+    console.log(details);
+    if (details.type === "main_frame") {
+      const currentUrl = details.url;
+      if ((!details.originUrl || !twitterRegex.test(details.originUrl)) && twitterRegex.test(currentUrl)) {
+        const parsedUrl = new URL(currentUrl);
+        const params = new URLSearchParams(parsedUrl.search);
+        if (params.get('no-redirect') === 'true') {
+          return { cancel: true };
+        } else {
+          return { redirectUrl: targetURL };
+        }
+      }
     }
-  }
-});
+  },
+  { urls: ["*://*/*"] },
+  ["blocking"]
+);
