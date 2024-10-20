c5e975a550e80a586c935b620f1fb1b33bd4ef31
MidnightWolf420 <92683505+MidnightWolf420@users.noreply.github.com>
2024-10-19 22:29:33 -0400

    Now sends you to the search page if you were on someone's profile

--- chrome/background.js
+++ chrome/background.js
@@ -1,22 +1,25 @@
-const targetURL = "https://bsky.app/";
-const twitterRegex = /https?:\/\/(x|twitter)\.com/;
+const targetURL = "https://bsky.app";
+const twitterRegex = /https?:\/\/([a-z0-9]\.)?(x|twitter)\.com(\/)?/;
+const pages = ["home", "search", "explore", "settings", "messages", "notifications"];
 
-chrome.webRequest.onBeforeRequest.addListener(
-  async (details) => {
-    console.log(details);
-    if (details.type === "main_frame") {
-      const currentUrl = details.url;
-      if ((!details.originUrl || !twitterRegex.test(details.originUrl)) && twitterRegex.test(currentUrl)) {
-        const parsedUrl = new URL(currentUrl);
-        const params = new URLSearchParams(parsedUrl.search);
-        if (params.get('no-redirect') === 'true') {
-          return { cancel: true };
-        } else {
-          return { redirectUrl: targetURL };
-        }
-      }
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && twitterRegex.test(tab.url)) {
+    const parsedUrl = new URL(tab.url);
+    const params = new URLSearchParams(parsedUrl.search);
+    if (!params.get('no-redirect')) {
+        let path = parsedUrl.pathname.split('/')[1];
+        if(/https?:\/\/(x|twitter)\.com\/[a-zA-Z0-9-_]+$/i && !new RegExp(`(${pages.join("|")})`, "i").test(path)) {
+            chrome.tabs.update(tabId, { url: `${targetURL}/search?q=${path}` });
+        } else if(/https?:\/\/(x|twitter)\.com\/search(\/)?\?q=/i) {
+            chrome.tabs.update(tabId, { url: `${targetURL}/search?q=${params.get('q')||""}` });
+        } else if(/https?:\/\/(x|twitter)\.com\/settings/i) {
+            chrome.tabs.update(tabId, { url: `${targetURL}/settings` });
+        } else if(/https?:\/\/(x|twitter)\.com\/messages/i) {
+            chrome.tabs.update(tabId, { url: `${targetURL}/messages` });
+        } else if(/https?:\/\/(x|twitter)\.com\/notifications/i) {
+            chrome.tabs.update(tabId, { url: `${targetURL}/notifications` });
+        } else chrome.tabs.update(tabId, { url: targetURL });
+        
     }
-  },
-  { urls: ["*://*/*"] },
-  ["blocking"]
-);
+  }
+});
--- firefox/background.js
+++ firefox/background.js
@@ -1,17 +1,31 @@
-const targetURL = "https://bsky.app/";
-const twitterRegex = /https?:\/\/(x|twitter)\.com/;
+const targetURL = "https://bsky.app";
+const twitterRegex = /https?:\/\/(x|twitter)\.com(\/)?/;
+const twitterSubdomainRegex = /https?:\/\/([a-z0-9]\.)?(x|twitter)\.com(\/)?/;
+const pages = ["home", "search", "explore", "settings", "messages", "notifications"];
 
 browser.webRequest.onBeforeRequest.addListener(
   async (details) => {
     if(details.type == "main_frame") {
         const currentUrl = details.url;
-        if ((!details.originUrl || !twitterRegex.test(details.originUrl)) && twitterRegex.test(currentUrl)) {
+        if ((!details.originUrl || !twitterRegex.test(details.originUrl)) && (twitterRegex.test(currentUrl) || twitterSubdomainRegex.test(currentUrl))) {
             const parsedUrl = new URL(currentUrl);
             const params = new URLSearchParams(parsedUrl.search);
-            if (params.get('no-redirect') || params.get('no-redirect') === 'true') {
+            if (params.get('no-redirect')) {
+                console.log("No redirect")
                 return {};
             } else {
-                return { redirectUrl: targetURL };
+                let path = parsedUrl.pathname.split('/')[1];
+                if(/https?:\/\/(x|twitter)\.com\/[a-zA-Z0-9-_]+$/i && !new RegExp(`(${pages.join("|")})`, "i").test(path)) {
+                    return { redirectUrl: `${targetURL}/search?q=${path}` };
+                } else if(/https?:\/\/(x|twitter)\.com\/search(\/)?\?q=/i) {
+                    return { redirectUrl: `${targetURL}/search?q=${params.get('q')||""}` };
+                } else if(/https?:\/\/(x|twitter)\.com\/settings/i) {
+                    return { redirectUrl: `${targetURL}/settings` };
+                } else if(/https?:\/\/(x|twitter)\.com\/messages/i) {
+                    return { redirectUrl: `${targetURL}/messages` };
+                } else if(/https?:\/\/(x|twitter)\.com\/notifications/i) {
+                    return { redirectUrl: `${targetURL}/notifications` };
+                } else return { redirectUrl: targetURL };
             }
         }
     }
